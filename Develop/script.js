d3d11a74c980f45a2ac1309548e17e2d7df1e5cd
Desmond <desmondaldridge@gmail.com>
2020-08-07 14:28:45 -0700

    Realized a mistake, but left it up to remember my line of thinking later on in the course

--- Develop/script.js
+++ Develop/script.js
@@ -18,7 +18,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Choices
 
-var length = [];
+var length = []; //oh I guess I never had to declare this... I'm going to leave it up to look back later to try to remember my early thought processes
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var specialChar = ['!','@','#','$','%','^','&','*'];
